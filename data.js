89eb4a88ff9d88c28441645606ad6364f896f33a
Dmitry Drevich <jeidan2012@gmail.com>
2023-09-23 20:10:48 +0300

    feat: add volume and progress bar

---
+++ data.js
@@ -0,0 +1,20 @@
+let songs = [
+    {
+        name: 'Synthetic Nights',
+        path: "./assets/songs/Marvel83' - Synthetic Nights.mp3",
+        artist: "Marvel83'",
+        cover: './assets/img/Synthetic Nights.jpg'
+    },
+    {
+        name: 'Runaway (feat. Waves-On-Waves)',
+        path: "./assets/songs/Marvel83' - Runaway (feat. Waves-On-Waves).mp3",
+        artist: "Marvel83'",
+        cover: './assets/img/runaway.jpg'
+    },
+    {
+        name: 'Atlantis',
+        path: "./assets/songs/Marvel83' - Atlantis.mp3",
+        artist: "Marvel83'",
+        cover: './assets/img/atlantis.jpeg.jpg'
+    },
+]
